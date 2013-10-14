e1e05d643c45943e7752a12069695260e9795999
DeProdigy <alex.hint.ah@gmail.com>
2013-10-14 18:10:42 -0400

    tested the routes

--- main.rb
+++ main.rb
@@ -1,21 +1,62 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'pry'
+require 'active_record'
+
+
+ActiveRecord::Base.logger = Logger.new( STDOUT )
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "postgresql",
+  :host => "ec2-54-225-112-205.compute-1.amazonaws.com",
+  :username => "fvpqhfzusshfch",
+  :password => "Q787hi9QR6c2Y4CtFJEETXgWGt",
+  :database => "ddut7v6pj5tbqu"
+)
+
+helpers do
+  def before
+    ActiveRecord::Base.logger = Logger.new( STDOUT )
+    ActiveRecord::Base.establish_connection(
+      :adapter => "postgresql",
+      :host => "ec2-54-225-112-205.compute-1.amazonaws.com",
+      :username => "fvpqhfzusshfch",
+      :password => "Q787hi9QR6c2Y4CtFJEETXgWGt",
+      :database => "ddut7v6pj5tbqu"
+    )
+  end
+  def after
+    ActiveRecord::Base.connection.close
+  end
+end
+
+#ActiveRecord::Base.connection.close
+
+class Comment < ActiveRecord::Base
+end
+
+
 
 get '/' do
   redirect to('/comments')
 end
 
 get '/comments' do
+  before
+  @comments = Comment.all
+  #binding.pry
+  after
   erb :comments
+end
+
+get '/comments/new' do
+  erb :comments_new
 end
 
 get '/comments/:id' do
   erb :comments_id
 end
 
-get '/comments/new' do
-  erb :comments_new
-end
 
 post '/comments' do
   redirect to('/comments/:id')
@@ -24,3 +65,8 @@ end
 post '/comments/:id' do
   redirect to('/comments/:id')
 end
+
+
+
+
+
